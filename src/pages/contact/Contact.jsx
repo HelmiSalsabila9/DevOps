28337782e052cb517855eca95bf65a83aaf0e0a6
HelmiSalsabila9 <helmisalsabila9@gmail.com>
2022-10-26 16:50:49 +0700

    DevOps-Update

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -1,23 +1,28 @@
 import Header from '../../components/Header'
 import HeaderImage from '../../images/header_kontak.png'
-import {MdEmail} from 'react-icons/md'
-// import {BsMessenger} from 'react-icons/bs'
-import {IoLogoWhatsapp} from 'react-icons/io'
+import {AiOutlineMail} from 'react-icons/ai'
+import {BsWhatsapp} from 'react-icons/bs'
+import {BsInstagram} from 'react-icons/bs'
+import {BsLinkedin} from 'react-icons/bs'
+import {FaTiktok} from 'react-icons/fa'
 
 import './contact.css'
 
 const Contact = () => {
   return (
     <>
-      <Header title="Berhubungan" image={HeaderImage}>
+      <Header title="Kontak & Media Sosial" image={HeaderImage}>
         Menerima bekerja sama dalam hal bisnis ataupun yang lainnya serta kritik dan saran agar website yang saya buat jauh lebih baik lagi. Silakan hubungi yang ada di bawah ini:  
       </Header>
       <section className='contact'>
         <div className="container contact__container">
           <div className="contact__wrapper">
-            <a href="mailto:helmisalsabila70@gmail.com" target='_blank' rel='noreferrer noopener'><MdEmail/></a>
-            {/* <a href="https://m.me/hels" target='_blank' rel='noreferrer noopener'><BsMessenger/></a> */}
-            <a href="https://wa.me/+6285900386218" target='_blank' rel='noreferrer noopener'><IoLogoWhatsapp/></a>
+            <a href="mailto:helmisalsabila70@gmail.com" target='_blank' rel='noreferrer noopener'><AiOutlineMail/></a>
+            <a href="https://wa.me/+6285900386218" target='_blank' rel='noreferrer noopener'><BsWhatsapp/></a>
+            <a href="https://instagram.com/hels.ae" target='_blank' rel='noreferrer noopener'><BsInstagram/></a>
+            <a href="https://www.tiktok.com/@hels.id" target='_blank' rel='noreferrer noopener'><FaTiktok/></a>
+            <a href="https://lingkedin.com/in/helmisalsabila9/" target='_blank' rel='noreferrer noopener'><BsLinkedin/></a>
+
           </div>
         </div>
       </section>
